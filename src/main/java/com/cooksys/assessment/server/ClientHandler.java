917e61fe8e58d05cf54143f1c443b0d5da75f498
DoingItGonzo <travis.pettrey@gmail.com>
2017-09-06 18:57:24 -0500

    Have broadcast and users working. Might actually get some sleep tonight

--- src/main/java/com/cooksys/assessment/server/ClientHandler.java
+++ src/main/java/com/cooksys/assessment/server/ClientHandler.java
@@ -6,17 +6,28 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.cooksys.assessment.model.Message;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ClientHandler implements Runnable {
-	private Logger log = LoggerFactory.getLogger(ClientHandler.class);
 
-	private Socket socket;
+	private Logger log = LoggerFactory.getLogger(ClientHandler.class);
+	
+//	static HashMap<String, ClientHandler> clientMap = new HashMap<String, ClientHandler>();
+	static ConcurrentHashMap<String, ClientHandler> clientMap = new ConcurrentHashMap<String, ClientHandler>();
+	
+	public ObjectMapper mapper;
+	public PrintWriter writer;
+	public Message message;
+	public Socket socket;
+	
 
 	public ClientHandler(Socket socket) {
 		super();
@@ -26,34 +37,63 @@ public class ClientHandler implements Runnable {
 	public void run() {
 		try {
 
-			ObjectMapper mapper = new ObjectMapper();
+			mapper = new ObjectMapper();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
 			while (!socket.isClosed()) {
 				String raw = reader.readLine();
-				Message message = mapper.readValue(raw, Message.class);
-
+				message = mapper.readValue(raw, Message.class);
+				
+				// add error handling for multiple copies of a userName  usernames 
 				switch (message.getCommand()) {
+				
 					case "connect":
 						log.info("user <{}> connected", message.getUsername());
+						clientMap.put(message.getUsername(), this);
 						break;
+						
 					case "disconnect":
 						log.info("user <{}> disconnected", message.getUsername());
 						this.socket.close();
 						break;
+						
 					case "echo":
 						log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
-						String response = mapper.writeValueAsString(message);
-						writer.write(response);
-						writer.flush();
-						break;
+						messageOut(message.getContents(), this);
+						break;	
+						
+					case "broadcast":
+                        log.info("user <{}> broadcasted message <{}>", message.getUsername(), message.getContents());
+                        for(ClientHandler client : clientMap.values()){
+                        	messageOut(message.getUsername() + "(all) " + message.getContents(), client);
+                        }
+                        break;
+
+					case "users":
+						log.info("user <{}> used command<{}>", message.getUsername(), message.getCommand());
+						String userList = "";
+						for(String userName : clientMap.keySet()){
+                        	userList += (userName + "\n");
+                        }
+						messageOut(userList, this);
+						break;	
 				}
 			}
 
 		} catch (IOException e) {
 			log.error("Something went wrong :/", e);
 		}
+		
 	}
+
+	public void messageOut(String outputMessage, ClientHandler client) throws JsonProcessingException {
+		Message out = new Message();
+		out.setContents(outputMessage);
+		String outJSON = client.mapper.writeValueAsString(out);
+		client.writer.write(outJSON);
+		client.writer.flush();
+
+    }
 
 }
